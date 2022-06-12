092e2a5c18ccd8bd1d4387bd0ec3860da3f5dff5
Geovani Paz <geovanipaz7@gmail.com>
2022-06-12 15:28:34 -0300

    sistema de checkout e pagamento

--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -1,3 +1,94 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+#cliente
+class Customer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
+    name = models.CharField(max_length=200, null=True)
+    email = models.CharField(max_length=200, null=True)
+    
+    def __str__(self):
+        return self.name
+    
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.FloatField()
+    digital = models.BooleanField(default=False, blank=True, null=True)
+    image = models.ImageField(blank=True, null=True)
+    
+    def __str__(self) -> str:
+ 
+        return self.name
+    
+'''
+PEDIDO
+- cliente pode ter um vários pedidos 
+ex: pedido(cliente_id=1)
+'''
+class Order(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, 
+                                 null=True, blank=True)
+    date_ordered = models.DateTimeField(auto_now_add=True)
+    complete = models.BooleanField(default=False)
+    transaction_id = models.CharField(max_length=100, null=True)
+    
+    def __str__(self) -> str:
+        return str(self.id)
+    
+    @property
+    def shipping(self):
+        shipping = False
+        orderitems = self.orderitem_set.all()
+        for i in orderitems:
+            if i.product.digital == False:
+                shipping = True
+        return shipping
+    
+    # pega soma dos valores dos item do pedido.
+    # baseado em quais 'item do pedido' estão o 'id do pedido'
+    @property
+    def get_cart_total(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.get_total for item in orderitems])
+        return total
+    
+    @property
+    def get_cart_items(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.quantity for item in orderitems])
+        return total
+'''
+ITEM DO PEDIDO
+- um produto pode está em vários item do pedido
+ex: itempedido(produto_id=1)
+- um pedido pode ter vários item do pedido
+ex:itempedido(pedido_id=2)
+'''
+class OrderItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+    quantity = models.IntegerField(default=0, null=True, blank=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+    
+    @property
+    def get_total(self):
+        total = self.product.price * self.quantity
+        return total
+    
+'''
+ENDEREÇO DE ENTREGA
+- um endereço de entrega tem um cliente e um pedido
+'''    
+class ShippingAddress(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+    address = models.CharField(max_length=200, null=True)
+    city = models.CharField(max_length=200, null=True)
+    state = models.CharField(max_length=200, null=True)
+    zipcode = models.CharField(max_length=200, null=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.address
+    
--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -1,14 +1,114 @@
 from multiprocessing import context
 from django.shortcuts import render
+from .models import Order, OrderItem, Product, ShippingAddress
+from django.http import JsonResponse
+import json
+import datetime
 
 def store(request):
-    context = {}
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        #pega ou cria um pedido
+        order, created = Order.objects.get_or_create(
+            customer=customer, complete=False
+        )
+        #coloca numa lista todos item do pedido do pedido
+        items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
+    else:
+        items = []
+        order = {'get_cart_total':0, 'get_total_items':0,'shipping':False}
+        cartItems = order['get_total_items']
+    
+    products = Product.objects.all()
+    context = {'products':products, 'cartItems':cartItems}
     return render(request, 'store/store.html', context)
 
 def cart(request):
-    context = {}
+    #se autenticado o cliente é o atual usuario
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        #pega ou cria um pedido
+        order, created = Order.objects.get_or_create(
+            customer=customer, complete=False
+        )
+        #coloca numa lista todos item do pedido do pedido
+        items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
+    else:
+        items = []
+        order = {'get_cart_total':0, 'get_total_items':0, 'shipping':False}
+        cartItems = order['get_total_items']
+        
+    context = {'items':items, 'order':order, 'cartItems':cartItems}
     return render(request, 'store/cart.html', context)
 
 def checkout(request):
-    context = {}
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(
+            customer=customer, complete=False)
+        items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
+    else:
+        items = []
+        order = {'get_cart_total':0, 'get_total_items':0, 'shipping':False}
+        cartItems = order['get_total_items']
+        
+    context = {'items':items, 'order':order,'cartItems':cartItems}
     return render(request, 'store/checkout.html', context)
+
+def updateItem(request):
+    data = json.loads(request.body)
+    productId = data['productId']
+    action = data['action']
+    
+    print('action:', action)
+    print('productId:', productId)
+    
+    customer = request.user.customer
+    product = Product.objects.get(pk=productId)
+    order, created = Order.objects.get_or_create(
+            customer=customer, complete=False)
+    
+    orderitem, created = OrderItem.objects.get_or_create(
+        order=order, product=product)
+    
+    if action == 'add':
+        orderitem.quantity = (orderitem.quantity + 1)
+    elif action == 'remove':
+        orderitem.quantity = (orderitem.quantity - 1)
+        
+    orderitem.save()
+    if orderitem.quantity <= 0:
+        orderitem.delete()
+        
+    return JsonResponse('Item was Added', safe=False)
+
+def processOrder(request):
+    transaction_id = datetime.datetime.now().timestamp()
+    data = json.loads(request.body)
+    
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(
+            customer=customer, complete=False)
+        total = float(data['form']['total'])
+        order.transaction_id = transaction_id
+        
+        if total == order.get_cart_total:
+            order.complete = True
+        order.save()
+        
+        if order.shipping == True:
+            ShippingAddress.objects.create(
+			customer=customer,
+			order=order,
+			address=data['shipping']['address'],
+			city=data['shipping']['city'],
+			state=data['shipping']['state'],
+			zipcode=data['shipping']['zipcode'],
+			)
+    else:
+        print('User is not logged in..')
+    return JsonResponse('Payment Completed', safe=False)
